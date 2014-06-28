00052439d06461cac4f4e63ea7523a0a2abc0d46
mani <mani95lisa@gmail.com>
2014-06-28 16:29:22 +0800

    fix parse bug

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 (function(module) {
     "use strict";
     var TuDou = {},
-        embed = '<div class="video-container"><iframe class="tudou-plugin" src="http://www.tudou.com/programs/view/html5embed.action?type=0&code=$1&lcode=&resourceId=339959839_06_05_99" allowtransparency="true" scrolling="no" border="0" frameborder="0"></iframe></div>';
+        embed = '<div class="video-container"><iframe class="tudou-plugin" src="http://www.tudou.com/programs/view/html5embed.action?type=0&code=$2&lcode=&resourceId=339959839_06_05_99" allowtransparency="true" scrolling="no" border="0" frameborder="0"></iframe></div>';
 
     TuDou.parse = function(postContent, callback) {
-        var	regularUrl = /(?:http?:\/\/)www\.tudou.com\/programs\/view\/([\w]+)\//;
-
-        if (postContent.match(regularUrl)) {
-            postContent = postContent.replace(regularUrl, embed);
+        var re = /(<p><a href=")http?:\/\/www\.tudou.com\/programs\/view\/(([\w]+))\/.*?<\/a><\/p>/gm;
+        if (re.test(postContent) !== false) {
+            postContent = postContent.replace(re, embed);
         }
         callback(null, postContent);
     };
